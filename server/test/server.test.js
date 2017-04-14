bd9627d9b979591a4ca1511a48c9237486e173fb
Idan <idanbusso@gmail.com>
2017-04-15 01:15:52 +0300

    Add tests for PATCH /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 const request = require('supertest');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../model/todo');
@@ -12,7 +12,9 @@ const todosMock = [
     },
     {
         _id: new ObjectID(),
-        text: 'Second test todo'
+        text: 'Second test todo',
+        completed: true,
+        completedAt: 333
     }
 ];
 
@@ -38,7 +40,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find({text}).then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[ 0 ].text).toEqual(text);
                     done();
@@ -82,13 +84,13 @@ describe('GET /todos', () => {
 
 describe('GET /todos/:id', () => {
     it('should get a todo by id', (done) => {
-        const id = todosMock[0]._id.toHexString();
+        const id = todosMock[ 0 ]._id.toHexString();
 
         request(app)
             .get(`/todos/${id}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.todo.text).toBe(todosMock[0].text);
+                expect(res.body.todo.text).toBe(todosMock[ 0 ].text);
             })
             .end(done);
     });
@@ -120,7 +122,7 @@ describe('GET /todos/:id', () => {
 
 describe('DELETE /todos/:id', () => {
     it('should remove a todo', (done) => {
-        const hexId = todosMock[1]._id.toHexString();
+        const hexId = todosMock[ 1 ]._id.toHexString();
 
         request(app)
             .delete(`/todos/${hexId}`)
@@ -157,6 +159,83 @@ describe('DELETE /todos/:id', () => {
 
         request(app)
             .delete(`/todos/${id}`)
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toEqual({});
+            })
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update a todo', (done) => {
+        const hexId = todosMock[ 0 ]._id.toHexString();
+        const text = 'New todo text value';
+        const completed = true;
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                text,
+                completed
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(completed);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toInclude({
+                        text,
+                        completed
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        const hexId = todosMock[ 1 ]._id.toHexString();
+        const completed = false;
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({ completed })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+                expect(res.body.todo.completed).toBe(completed);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        const id = new ObjectID("68f0f0f781fdb2155434b6db").toHexString();
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({})
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toEqual({});
+            })
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        const id = "123abc";
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({})
             .expect(404)
             .expect((res) => {
                 expect(res.body).toEqual({});
