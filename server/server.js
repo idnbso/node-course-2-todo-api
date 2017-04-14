f6571183bb8c6880292d2cbdb106ad9f9c7db3a4
Idan <idanbusso@gmail.com>
2017-04-14 22:35:39 +0300

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -45,6 +45,20 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findOneAndRemove({ _id: id }).then((todo) => {
+        todo && res.send({ todo });
+        res.status(404).send();
+    }, (e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
